300041020d59bf55f8c66d0ac9bdf1bd937a94de
Kebzzang <kebinkorea@gmail.com>
2021-10-27 02:55:41 +0900

    added some ideas on OOP

--- project.py
+++ project.py
@@ -2,10 +2,25 @@
 # 학생 포멧에 맞춰서 프린트하기
 # class 형식
 # 우리는 객체지향적인 삶을 삽시다
+#  정말 이렇게 스튜던트 인포 하나만 띡 만들거야?
+# 클래스는 여러 개로 분리해야 하지 않겠니?
+## class Student
+## class Database
+### Database has students
+### has a 관계 그림을 생각해
+### https://wikidocs.net/21053
+
+### https://jinmay.github.io/2019/11/23/python/python-class-first/
+
+### https://www.daleseo.com/python-property/
 
 class Student:
     def __init__(self):
-        self
+        # 학생 한줄씩 만드는 거야
+        pass
+class Database:
+    def __init__(self, filename='Student.txt'):
+        pass
 
     def show(self):
         pass
@@ -30,12 +45,14 @@ class Student:
         pass
 
 
-def main():
-    fr = open("students.txt", "r")
-    # 한 학생씩 읽어서 \t 삭제시켜주고, 각 학생 이름의 리스트에 성적 전부 넣어주기
-    print(fr.read())
 
-    fr.close()
+
+def main():
+   while True:
+       command=input('command: ')
+       if command=='quit':
+           break
+
 
 
 main()
